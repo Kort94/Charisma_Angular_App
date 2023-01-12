631cbb3b3282f9c0fb1004cc9687d0fc236b66fd
Kort94 <katlehosenkoto@gmail.com>
2023-01-12 15:11:30 +0200

    Added content on the swiper slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { SwiperModule } from 'swiper/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SwiperModule
+    SwiperModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,25 +1,48 @@
 import { Component, ViewEncapsulation, OnInit } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-
 import SwiperCore, { Keyboard, Pagination, Navigation, Virtual } from 'swiper';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 SwiperCore.use([Keyboard, Pagination, Navigation, Virtual]);
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+  styleUrls: ['./home.component.scss', '../../styles.scss'],
+  animations: [
+    trigger('fade', [ 
+      transition('void => *', [
+        style({ opacity: 1 }), 
+        animate('300ms ease-in')
+      ]) 
+    ])
+  ],
   encapsulation: ViewEncapsulation.None,
 })
 export class HomeComponent {
 
-  slides$ = new BehaviorSubject<string[]>(['']);
-
   
+  SlideContent = [
+    {imageURL: 'url(../../assets/img/bg-img/bg-.jpg)',
+    h5Content: 'Recognise',
+    h3Content: 'We are a doctrinal and dynamic household of faith with a passion to:',
+    h2Content: 'Mature saints in CHRIST',
+    classc: 'justify-content-end'
+    },
+    {imageURL: 'url(../../assets/img/bg-img/bg-three.jpg)',
+    h5Content: 'Raise',
+    h2Content: 'Mature leaders in CHRIST',
+    classc: 'justify-content-end'
+  },
+    {imageURL: 'url(../../assets/img/bg-img/bg-six.jpg)',
+    h5Content: 'Release',
+    h2Content: 'Mature churches and ministries in CHRIST',
+    classc: ''
+  },
+  ]
 
-  ngOnInit(): void {
-    this.slides$.next(
-      Array.from({ length: 3 }).map((el, index) => `Slide ${index + 1}`)
-    );
-  }
+  //ngOnInit(): void {
+   // this.slides$.next(
+     // Array.from({ length: 3 }).map((el, index) => `Slide ${index + 1}`)
+    //);
+ // }
 }
